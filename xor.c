502c56df77b4bbe8badc66ec95732f5ccd71463b
Ouakki Abderrahim <aouakki@SA203-07A.ensi-bourges.fr>
2017-09-13 17:31:36 +0200

    add xor algo

---
+++ xor.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+	FILE *fi,*fo ; 
+	char *cp ; 
+	int c ; 
+
+	if(cp = argv[1]) {
+		if((fi=fopen(argv[2],"rb")) != NULL){
+			if((fo=fopen(argv[3],"wb")) != NULL){
+				while((c=getc(fi)) !=EOF){
+					if(*cp == '\0') cp = argv[1] ; 
+					c^=*(cp++) ; 
+					putc(c,fo) ;
+				}
+				fclose(fo) ; 
+			}
+			fclose(fi) ; 
+		}
+	}
+	return 1 ; 
+}
